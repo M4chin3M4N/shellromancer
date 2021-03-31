690bb75fc3b8e9714aa090e68b1eb782225aba26
M4chin3M4N <75846902+M4chin3M4N@users.noreply.github.com>
2021-03-31 14:50:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+if os.getuid() != 0:
+  print("[!]You must execute as root!!!!!")
+  print("Say the magic word ")
+  sys.exit()
+  
+step1 = f"chmod +x shellromancer.py"
+step2 = f"mv shellromancer.py /usr/bin/shellromancer"
+step3 = f"mv payload.py /usr/bin"
+
+os.system(step1)
+os.system(step2)
+os.system(step3)
+
+print("Done")
